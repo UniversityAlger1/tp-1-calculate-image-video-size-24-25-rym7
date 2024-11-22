5be672d5dec1e26739aaed779d026a0cd3cb12b0
rym7 <rymrahmatrabelsi@gmail.com>
2024-11-22 18:49:17 +0100

    completer les exos

--- grayScaleImage.c
+++ grayScaleImage.c
@@ -6,7 +6,7 @@
 //   grayScale size of the image Bitmap (in byte)
 float grayScaleImage(int w, int h) {
    // YOUR CODE HERE - BEGIN
-
+   float size = w * h;
    // YOUR CODE HERE - END
-   return 0;
+   return size;
 }
